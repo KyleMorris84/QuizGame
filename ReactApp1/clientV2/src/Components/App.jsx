fa9f745844fd3bf63b178dfac1511d20f9f97a3a
Kyle <kylemorris8484@gmail.com>
2025-04-19 09:11:11 +0100

    Home button

--- ReactApp1/clientV2/src/Components/App.jsx
+++ ReactApp1/clientV2/src/Components/App.jsx
@@ -9,6 +9,8 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#f8f7f4] text-gray-800 font-['Poppins',sans-serif] px-4">
       
+      <a href="/"><img className="w-12 absolute inset-x-3 inset-y-3"src="home.svg" /></a>
+
       { currentPage == "home" && 
         <Home setCurrentPage={setCurrentPage} />
       }
