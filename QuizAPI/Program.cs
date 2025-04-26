8554d394b808938e82e73db3b348e92f43f9f066
Kyle <kylemorris8484@gmail.com>
2025-04-26 11:09:14 +0100

    Small program.cs changes

--- QuizAPI/Program.cs
+++ QuizAPI/Program.cs
@@ -3,6 +3,8 @@ using ReactApp1.Server.Models;
 using ReactApp1.Server.Models.Repositories;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var connectionString = "Data Source=" + 
+    Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "AppData", "quiz-db.db");
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,18 +21,8 @@ builder.Services.AddCors(options =>
                             .AllowAnyHeader();
                       });
 });
-
-// Add services to the container.
-
-string dbPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "AppData", "quiz-db.db");
-
-var connectionString = $"Data Source={dbPath}";
-
-Console.WriteLine(connectionString);
-
 builder.Services.AddDbContext<QuizDbContext>(options =>
     options.UseSqlite(connectionString));
-
 builder.Services.AddControllers();
 builder.Services.AddScoped<IGenreRepository, GenreRepository>();
 builder.Services.AddScoped<IQuizRepository, QuizRepository>();
@@ -40,13 +32,9 @@ var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
 app.UseHttpsRedirection();
-
 app.UseCors(MyAllowSpecificOrigins);
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
